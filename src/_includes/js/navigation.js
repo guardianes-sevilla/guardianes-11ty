894c575e0063f197813b1d52663a1f1c5939c7c4
Juan D. Jara <juanorigami@gmail.com>
2019-10-19 20:46:41 +0200

    fixing navigation and images

--- src/_includes/js/navigation.js
+++ src/_includes/js/navigation.js
@@ -2,55 +2,56 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   // var $btn = document.querySelectorAll('nav button');
-  var $vlinks = document.querySelector('nav ul');
-  var $hlinks = document.querySelector('nav .more ul');
+  var $visibleLinks = document.querySelector('nav ul');
+  var $hiddenLinks  = document.querySelector('nav .more ul');
 
-  var numOfItems = 0;
-  var totalSpace = 0;
+  // var numOfItems = 0;
   var breakWidths = [];
+  var _totalSpace = 0;
 
-  for (const li of $vlinks.children) {
-    if (li === $vlinks.children.lastChild) {
+  for (const li of $visibleLinks.children) {
+    if (li === $visibleLinks.children.lastChild) {
       continue;
     }
-    totalSpace += li.offsetWidth
-    numOfItems++
-    breakWidths.push(totalSpace)
+    _totalSpace += li.offsetWidth
+    // numOfItems++
+    breakWidths.push(_totalSpace)
   }
 
-  var availableSpace, numOfVisibleItems, requiredSpace;
-
-  function checkSpace() {
-
-    // Get instant state
-    availableSpace = $vlinks.offsetWidth - 10;
-    numOfVisibleItems = $vlinks.children.length;
-    requiredSpace = breakWidths[numOfVisibleItems - 1];
-
-    // Not enough space
-    if (requiredSpace > availableSpace) {
-      $hlinks.prepend($vlinks.children[$vlinks.children.length - 2])
-      numOfVisibleItems -= 1;
-      checkSpace();
-
-      // More than enough space
-    } else if (availableSpace > breakWidths[numOfVisibleItems]) {
-      $vlinks.insertBefore($hlinks.children[0], $vlinks.lastChild)
-      numOfVisibleItems += 1;
-      if (availableSpace > breakWidths[numOfVisibleItems]) {
-        checkSpace()
-      }
+  function moveToHidden () {
+    var target = $visibleLinks.children[$visibleLinks.children.length - 2]
+    if (target) {
+      $hiddenLinks.prepend(target)
     }
-    // Update the button
-    // $btn.attr("count", numOfItems - numOfVisibleItems);
-    // if (numOfVisibleItems === numOfItems) {
-    //   $btn.addClass('hidden');
-    // } else $btn.removeClass('hidden');
   }
 
-  // $btn.on('click', function() {
-  //   $hlinks.toggleClass('hidden');
-  // });
+  function moveToVisible () {
+    var target = $hiddenLinks.children[0]
+    if (target) {
+      $visibleLinks.insertBefore(target, $visibleLinks.lastChild)
+    }
+  }
+
+  function checkSpace () {
+    var mediaQuery = window.matchMedia('(min-width: 600px)').matches
+    breakWidths.forEach(currentBreak => {
+      var numOfVisibleItems = $visibleLinks.children.length;
+      var availableSpace = $visibleLinks.offsetWidth - 10;
+      var requiredSpace = breakWidths[numOfVisibleItems - 1];
+
+      if (!mediaQuery) {
+        moveToHidden()
+      } else {
+        if (requiredSpace > availableSpace) {
+          moveToHidden();
+        }
+        if (availableSpace > currentBreak) {
+          moveToVisible()
+        }
+      }
+
+    })
+  }
 
   window.addEventListener('resize', checkSpace)
   checkSpace()
